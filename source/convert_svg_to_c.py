fc8279f4881af050b2ed9e6f53324b91ef8be367
Torsten Stremlau <torsten@stremlau.eu>
2021-08-15 21:01:53 +0200

    Allow XML transformations

--- source/convert_svg_to_c.py
+++ source/convert_svg_to_c.py
@@ -63,25 +63,77 @@ class Rectangle:
         self.h = float(h)
         self.color = int(color)
 
+    def normalize(self):
+        if self.w < 0:
+            self.x += self.w
+            self.w *= -1
+        if self.h < 0:
+            self.y += self.h
+            self.h *= -1
 
 
-def parseTranform(transform):
-    print(transform)
-    m = re.search("translate\((.*)\,(.*)\)", transform)
+def parseTranform(transformInput):
 
-    try:
-        off_x = float(m.group(1))
-        off_y = float(m.group(2))
-    except:
-        off_x = 0
-        off_y = 0
-
-    def translate(rect):
-        rect.x = rect.x + off_x
-        rect.y = rect.y + off_y
+    def nullTranform(rect):
         return rect
 
-    return translate
+    if transformInput is None:
+        return nullTranform
+
+    print(transformInput)
+    translate = re.search("translate\((.*)\,(.*)\)", transformInput)
+    scale1 = re.search("scale\(([^,]*)\)", transformInput)
+    scale2 = re.search("scale\((.*)\,(.*)\)", transformInput)
+
+    
+    if translate is not None:
+        try:
+            off_x = float(translate.group(1))
+            off_y = float(translate.group(2))
+        except:
+            off_x = 0
+            off_y = 0
+
+        def transform(rect):
+            rect.x = rect.x + off_x
+            rect.y = rect.y + off_y
+            return rect
+
+        return transform
+
+    if scale1 is not None:
+        try:
+            scale = float(scale1.group(1))
+        except:
+            scale = 1
+
+        def transform(rect):
+            rect.x = rect.x * scale
+            rect.y = rect.y * scale
+            rect.w = rect.w * scale
+            rect.h = rect.h * scale
+            return rect
+
+        return transform
+
+    if scale2 is not None:
+        try:
+            scale_x = float(scale2.group(1))
+            scale_y = float(scale2.group(2))
+        except:
+            scale_x = 1
+            scale_y = 1
+
+        def transform(rect):
+            rect.x = rect.x * scale_x
+            rect.y = rect.y * scale_y
+            rect.w = rect.w * scale_x
+            rect.h = rect.h * scale_y
+            return rect
+
+        return transform
+
+    
 
 
 def parseViewbox(viewBox):
@@ -132,9 +184,11 @@ def loadSvgRectangles(file_path, target_width, target_height):
         rect_elements = g.findall(".//svg:rect", ns)
         
         for rect in rect_elements:
+            rect_transform = parseTranform(rect.get("transform"))
             rgbiColor = parseStyle(rect.get("style"))
             rectangle = Rectangle(rect.get("x"), rect.get("y"),rect.get("width"), rect.get("height"), rgbiColor)
-            rectangle = viewbox_transform(global_transform(rectangle), target_width, target_height)
+            rectangle = viewbox_transform(global_transform(rect_transform(rectangle)), target_width, target_height)
+            rectangle.normalize()
             # transform = rect.get("transform")
             print("x:{} y:{} w:{} h:{}".format(rectangle.x,rectangle.y,rectangle.w,rectangle.h))
             rectangles.append(rectangle)
@@ -152,13 +206,15 @@ if __name__ == "__main__":
 
     rectangles  = loadSvgRectangles(args.input_svg, 320, 200)
 
-    for rect in rectangles:
-        print("drawRect({}, {}, {}, {}, {});".format(
-            int(rect.x),
-            int(rect.y),
-            int(rect.w),
-            int(rect.h),
-            rect.color))
+    with open(args.output_c, "w") as file:
+        for rect in rectangles:
+            file.write("drawRect({}, {}, {}, {}, {});\n".format(
+                round(rect.x),
+                round(rect.y),
+                round(rect.w),
+                round(rect.h),
+                rect.color))
+            
 
 
     print(args.input_svg, args.output_c)
